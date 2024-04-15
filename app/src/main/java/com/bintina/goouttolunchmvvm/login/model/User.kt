f3b397cd14795e0743cf12466b8743863524afa4
Bintina <105106238+Bintina@users.noreply.github.com>
2024-04-15 15:09:15 +0300

    Delete duplicate classes

--- app/src/main/java/com/bintina/goouttolunchmvvm/login/model/User.kt
+++ app/src/main/java/com/bintina/goouttolunchmvvm/login/model/User.kt
@@ -2,6 +2,7 @@ package com.bintina.goouttolunchmvvm.login.model
 
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 import com.bintina.goouttolunchmvvm.restaurants.model.Restaurant
 
@@ -12,7 +13,6 @@ data class User(
     val name: String? = null,
     val loginState: Int = 0,
     val profilePicture: String? = null,
-
     var restaurant: Restaurant? = null
 
 )

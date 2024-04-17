0064b3ead5e003c40e140bed82b3c6b3ce6396b8
Bintina <105106238+Bintina@users.noreply.github.com>
2024-04-17 08:05:55 +0300

    Restaurant List Adapter created

--- app/src/main/java/com/bintina/goouttolunchmvvm/MyApp.kt
+++ app/src/main/java/com/bintina/goouttolunchmvvm/MyApp.kt
@@ -1,11 +1,12 @@
 package com.bintina.goouttolunchmvvm
 
 import android.app.Application
-
-
+import com.bintina.goouttolunchmvvm.restaurants.model.Restaurant
 
 
 class MyApp: Application() {
 
-    companion object
+    companion object{
+        var restaurantList = mutableListOf<Restaurant>()
+    }
 }
--- app/src/main/java/com/bintina/goouttolunchmvvm/restaurants/list/view/adapter/Adapter.kt
+++ app/src/main/java/com/bintina/goouttolunchmvvm/restaurants/list/view/adapter/Adapter.kt
@@ -1,4 +1,71 @@
 package com.bintina.goouttolunchmvvm.restaurants.list.view.adapter
 
-class Adapter {
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import com.bintina.goouttolunchmvvm.MyApp
+import com.bintina.goouttolunchmvvm.R
+import com.bintina.goouttolunchmvvm.databinding.RestaurantItemViewBinding
+import com.bintina.goouttolunchmvvm.restaurants.list.view.OnRestaurantClickedListener
+import com.bintina.goouttolunchmvvm.restaurants.model.Restaurant
+import com.bumptech.glide.Glide
+
+class Adapter() : RecyclerView.Adapter<com.bintina.goouttolunchmvvm.restaurants.list.view.adapter.Adapter.ItemViewHolder>() {
+
+    val resaurantList = MyApp.restaurantList
+    lateinit var listener: OnRestaurantClickedListener
+
+    class ItemViewHolder(private val view: RestaurantItemViewBinding, private val context: Context) :
+        RecyclerView.ViewHolder(view.root) {
+        /**
+         * Binds the restaurant data to the view holder.
+         */
+        fun bind(restaurant: Restaurant, listener: OnRestaurantClickedListener) {
+            val restaurantId = restaurant.restaurantId.toString()
+            //clickedRestaurantLink = restaurantId
+
+
+
+            //Load image using Glide
+            val restaurantImageUrl = restaurant.photoUrl
+
+            Glide.with(view.restaurantImage.context)
+                .load(restaurantImageUrl)
+                .placeholder(R.drawable.hungry_droid)
+                .centerCrop()
+                .into(view.restaurantImage)
+
+            //Set Name
+            val restaurantName = restaurant.name
+            view.restaurantName.text = restaurantName
+
+            //Set Location and type
+            view.typeAndLocantion.text = restaurantName
+
+
+            //Set Caption View
+            view.openingHours.text = restaurantName
+
+            //Set click listener for News link
+            view.restaurantCard.setOnClickListener { listener.onRestaurantClick() }
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val binding = RestaurantItemViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        Log.d("RestaurantAdapterLog", "Restaurant adapter onCreateViewHolder called")
+        Log.d("RestaurantAdapterLog","result size = ${resaurantList.size}")
+        return ItemViewHolder(binding, parent.context)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        holder.bind(resaurantList[position], listener)
+    }
+
+    override fun getItemCount(): Int = resaurantList.size
+
 }

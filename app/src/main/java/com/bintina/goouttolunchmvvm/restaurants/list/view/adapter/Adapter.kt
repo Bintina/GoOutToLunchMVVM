5a93472bb014f228ef17f87bae529ef884bb24ee
Bintina <105106238+Bintina@users.noreply.github.com>
2024-04-19 12:01:39 +0300

    Resolved merge conflicts

--- app/src/main/java/com/bintina/goouttolunchmvvm/restaurants/list/view/adapter/Adapter.kt
+++ app/src/main/java/com/bintina/goouttolunchmvvm/restaurants/list/view/adapter/Adapter.kt
@@ -8,6 +8,9 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.Adapter
 import com.bintina.goouttolunchmvvm.MyApp
 import com.bintina.goouttolunchmvvm.R
+
+import com.bintina.goouttolunchmvvm.databinding.ItemRestaurantBinding
+
 import com.bintina.goouttolunchmvvm.databinding.RestaurantItemViewBinding
 import com.bintina.goouttolunchmvvm.restaurants.list.view.OnRestaurantClickedListener
 import com.bintina.goouttolunchmvvm.restaurants.model.Restaurant
@@ -18,7 +21,9 @@ class Adapter() : RecyclerView.Adapter<com.bintina.goouttolunchmvvm.restaurants.
     val resaurantList = MyApp.restaurantList
     lateinit var listener: OnRestaurantClickedListener
 
-    class ItemViewHolder(private val view: RestaurantItemViewBinding, private val context: Context) :
+
+    class ItemViewHolder(private val view: ItemRestaurantBinding, private val context: Context) :
+
         RecyclerView.ViewHolder(view.root) {
         /**
          * Binds the restaurant data to the view holder.
@@ -32,31 +37,34 @@ class Adapter() : RecyclerView.Adapter<com.bintina.goouttolunchmvvm.restaurants.
             //Load image using Glide
             val restaurantImageUrl = restaurant.photoUrl
 
-            Glide.with(view.restaurantImage.context)
+
+            Glide.with(view.ivPhotoRestaurant.context)
                 .load(restaurantImageUrl)
                 .placeholder(R.drawable.hungry_droid)
                 .centerCrop()
-                .into(view.restaurantImage)
+                .into(view.ivPhotoRestaurant)
 
             //Set Name
             val restaurantName = restaurant.name
-            view.restaurantName.text = restaurantName
+            view.tvRestaurantName.text = restaurantName
 
             //Set Location and type
-            view.typeAndLocantion.text = restaurantName
+            view.tvStyleAndAddress.text = restaurantName
 
 
             //Set Caption View
-            view.openingHours.text = restaurantName
+            view.tvOpeningHours.text = restaurantName
 
             //Set click listener for News link
-            view.restaurantCard.setOnClickListener { listener.onRestaurantClick() }
+            view.restaurantItem.setOnClickListener { listener.onRestaurantClick() }
 
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        val binding = RestaurantItemViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        val binding = ItemRestaurantBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
         Log.d("RestaurantAdapterLog", "Restaurant adapter onCreateViewHolder called")
         Log.d("RestaurantAdapterLog","result size = ${resaurantList.size}")
         return ItemViewHolder(binding, parent.context)

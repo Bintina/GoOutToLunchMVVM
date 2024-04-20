a6b68dff7f798831519d06b16af348b68ee317b1
Bintina <105106238+Bintina@users.noreply.github.com>
2024-04-20 11:44:49 +0300

    Restaurant and Coworker List Fragments linked to respective adapters

--- app/src/main/java/com/bintina/goouttolunchmvvm/coworkers/view/CoworkerListFragment.kt
+++ app/src/main/java/com/bintina/goouttolunchmvvm/coworkers/view/CoworkerListFragment.kt
@@ -1,7 +1,40 @@
 package com.bintina.goouttolunchmvvm.coworkers.view
 
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.bintina.goouttolunchmvvm.coworkers.view.adapter.Adapter
+import com.bintina.goouttolunchmvvm.databinding.FragmentCoworkersListBinding
 
-class CoworkerListFragment(): Fragment() {
+class CoworkerListFragment() : Fragment() {
+    lateinit var adapter: Adapter
 
+    private var _binding: FragmentCoworkersListBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentCoworkersListBinding.inflate(inflater, container, false)
+
+        initializeViews()
+
+        Log.d("CoworkerFragmentLog", "CoworkerListFragment inflated")
+        return binding.root
+    }
+
+    private fun initializeViews() {
+        adapter = Adapter()
+        binding.coworkerRecyclerContainer.adapter = adapter
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
--- app/src/main/java/com/bintina/goouttolunchmvvm/restaurants/list/RestaurantListFragment.kt
+++ app/src/main/java/com/bintina/goouttolunchmvvm/restaurants/list/RestaurantListFragment.kt
@@ -1,7 +1,43 @@
 package com.bintina.goouttolunchmvvm.restaurants.list
 
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.bintina.goouttolunchmvvm.databinding.FragmentRestaurantListBinding
+import com.bintina.goouttolunchmvvm.restaurants.list.view.adapter.Adapter
 
 class RestaurantListFragment(): Fragment(){
+
+    lateinit var adapter: Adapter
+
+    private var _binding: FragmentRestaurantListBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentRestaurantListBinding.inflate(inflater, container, false)
+
+        initializeViews()
+
+        Log.d("RestaurantListFragLog", "RestaurantListFragment inflated")
+
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    private fun initializeViews() {
+        adapter = Adapter()
+        binding.recyclerView.adapter = adapter
+    }
 
 }

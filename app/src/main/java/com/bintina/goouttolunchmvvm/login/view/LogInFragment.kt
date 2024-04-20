64bf87c4ffe3953aff45ab45af5625449dfc37dc
Bintina <105106238+Bintina@users.noreply.github.com>
2024-04-20 12:09:19 +0300

    Login Fragment and ViewModel Classes uncommented.

--- app/src/main/java/com/bintina/goouttolunchmvvm/login/view/LogInFragment.kt
+++ app/src/main/java/com/bintina/goouttolunchmvvm/login/view/LogInFragment.kt
@@ -1,31 +1,39 @@
 package com.bintina.goouttolunchmvvm.login.view//
 
-  /*  import android.os.Bundle
-    import android.view.LayoutInflater
-    import android.view.View
-    import android.view.ViewGroup
-    import androidx.fragment.app.Fragment
-    import androidx.lifecycle.Observer
-    import androidx.lifecycle.ViewModelProvider
-    import com.bintina.goouttolunchmvvm.databinding.FragmentLoginBinding
-    import com.bintina.goouttolunchmvvm.login.viewmodel.LogInViewModel
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.bintina.goouttolunchmvvm.databinding.FragmentLoginBinding
+import com.bintina.goouttolunchmvvm.login.viewmodel.LogInViewModel
 
 
+class LogInFragment : Fragment() {
 
-    class LogInFragment : Fragment() {
-        lateinit var binding: FragmentLoginBinding
-        lateinit var viewModel: LogInViewModel
-        override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-        ): View? {
-            binding = FragmentLoginBinding.inflate(layoutInflater)
-            viewModel = ViewModelProvider(this).get(LogInViewModel::class.java)
+    lateinit var viewModel: LogInViewModel
 
-            //viewModel.setUserName("Facebook Login")
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
 
-            return super.onCreateView(inflater, container, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(LogInViewModel::class.java)
 
-        }
-    }*/
+        //viewModel.setUserName("Facebook Login")
+        Log.d("LoginFragLog", "LoginFragment inflated")
+        return binding.root
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
--- app/src/main/java/com/bintina/goouttolunchmvvm/login/viewmodel/LoginViewModelFactory.kt
+++ app/src/main/java/com/bintina/goouttolunchmvvm/login/viewmodel/LoginViewModelFactory.kt
@@ -3,14 +3,13 @@ package com.bintina.goouttolunchmvvm.login.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
-/*
 
 class LoginViewModelFactory: ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
         if(modelClass.isAssignableFrom(LogInViewModel::class.java)){
-            return LogInViewModel(name) as T
+            return LogInViewModel() as T
         }
         throw IllegalArgumentException("ViewModel not found")
     }
 
-}*/
+}

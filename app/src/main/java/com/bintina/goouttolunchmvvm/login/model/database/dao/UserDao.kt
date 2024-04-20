2b621482bafc87f3f7fd0a2c3ebd3b401c52c33f
Bintina <105106238+Bintina@users.noreply.github.com>
2024-04-20 13:10:57 +0300

    Created Dao interfaces and Database Class

---
+++ app/src/main/java/com/bintina/goouttolunchmvvm/login/model/database/dao/UserDao.kt
@@ -0,0 +1,18 @@
+package com.bintina.goouttolunchmvvm.login.model.database.dao
+
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.OnConflictStrategy
+import android.arch.persistence.room.Query
+import androidx.lifecycle.LiveData
+import com.bintina.goouttolunchmvvm.login.model.User
+
+@Dao
+interface UserDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun createUser(user: User)
+
+    @Query("SELECT * FROM User WHERE id = :userId")
+    fun getUser(userId: Long): LiveData<User>
+    }
+}

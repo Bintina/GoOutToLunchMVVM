7b5263a8428e3062ea1332ba94bda13426628ac6
Bintina <105106238+Bintina@users.noreply.github.com>
2024-04-21 14:13:28 +0300

    Test runner class created

--- app/src/androidTest/java/com/bintina/goouttolunchmvvm/UserDaoInstrumentedTest.kt
+++ app/src/androidTest/java/com/bintina/goouttolunchmvvm/UserDaoInstrumentedTest.kt
@@ -1,4 +1,38 @@
 package com.bintina.goouttolunchmvvm
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.bintina.goouttolunchmvvm.login.model.database.SaveUserDatabase
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
 class UserDaoInstrumentedTest {
+    private var database: SaveUserDatabase.SaveUserDatabase? = null
+
+    @Rule
+    @JvmField
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    @Throws(Exception::class)
+    fun initDb() {
+        this.database = Room.inMemoryDatabaseBuilder(
+            getApplicationContext(),
+            SaveUserDatabase.SaveUserDatabase::class.java
+        )
+            .allowMainThreadQueries()
+            .build()
+    }
+
+    @After
+    @Throws(Exception::class)
+    fun closeDb(){
+        database?.close()
+    }
 }

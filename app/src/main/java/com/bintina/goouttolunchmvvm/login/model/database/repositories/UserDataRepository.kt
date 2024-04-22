7c38082db2a0719e00d3feacf590ca91739f89b6
Bintina <105106238+Bintina@users.noreply.github.com>
2024-04-22 10:25:42 +0300

    UserDataRepository created.

--- app/src/main/java/com/bintina/goouttolunchmvvm/login/model/database/repositories/UserDataRepository.kt
+++ app/src/main/java/com/bintina/goouttolunchmvvm/login/model/database/repositories/UserDataRepository.kt
@@ -1,4 +1,13 @@
 package com.bintina.goouttolunchmvvm.login.model.database.repositories
 
-class UserDataRepository {
+import androidx.lifecycle.LiveData
+import com.bintina.goouttolunchmvvm.login.model.User
+import com.bintina.goouttolunchmvvm.login.model.database.dao.UserDao
+
+class UserDataRepository(private val userDao: UserDao) {
+
+    //Get User
+    fun getUser(userId: Long): LiveData<User> {
+        return this.userDao.getUser(userId)
+    }
 }

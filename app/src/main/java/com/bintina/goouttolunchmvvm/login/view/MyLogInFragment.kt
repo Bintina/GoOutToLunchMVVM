f6404ab8c20e879c033d9cb5e07cf0d7e92eee6b
Bintina <105106238+Bintina@users.noreply.github.com>
2024-04-23 10:46:38 +0300

    MainActivity launchMode to standard

--- app/src/main/java/com/bintina/goouttolunchmvvm/login/view/MyLogInFragment.kt
+++ app/src/main/java/com/bintina/goouttolunchmvvm/login/view/MyLogInFragment.kt
@@ -9,11 +9,12 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.bintina.goouttolunchmvvm.databinding.FragmentLoginBinding
 import com.bintina.goouttolunchmvvm.login.viewmodel.LogInViewModel
+import com.bintina.goouttolunchmvvm.login.viewmodel.UserViewModel
 
 
 class LogInFragment : Fragment() {
 
-    lateinit var viewModel: LogInViewModel
+    lateinit var viewModel: UserViewModel
 
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
@@ -24,12 +25,18 @@ class LogInFragment : Fragment() {
     ): View {
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
 
-        viewModel = ViewModelProvider(this).get(LogInViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+        initializeViews()
 
         //viewModel.setUserName("Facebook Login")
         Log.d("LoginFragLog", "LoginFragment inflated")
         return binding.root
 
+    }
+
+    private fun initializeViews() {
+        binding.facebookBtn.text = "Login with Facebook"
+        binding.googleLogin.text = "Login with Google"
     }
 
     override fun onDestroy() {

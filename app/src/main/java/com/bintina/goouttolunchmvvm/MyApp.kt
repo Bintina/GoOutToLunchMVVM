3e3d485084e40442970176459412a1c946bb99c0
Bintina <105106238+Bintina@users.noreply.github.com>
2024-04-23 10:48:07 +0300

    Application Class updated with Context and instance

--- app/src/main/java/com/bintina/goouttolunchmvvm/MyApp.kt
+++ app/src/main/java/com/bintina/goouttolunchmvvm/MyApp.kt
@@ -1,6 +1,7 @@
 package com.bintina.goouttolunchmvvm
 
 import android.app.Application
+import android.content.Context
 import com.bintina.goouttolunchmvvm.login.model.User
 import com.bintina.goouttolunchmvvm.restaurants.model.Restaurant
 
@@ -8,7 +9,21 @@ import com.bintina.goouttolunchmvvm.restaurants.model.Restaurant
 class MyApp: Application() {
 
     companion object{
+
+        // Application instance
+        lateinit var instance: MyApp
+            private set
+
+        lateinit var myContext: Context
+
+
         var restaurantList = mutableListOf<Restaurant>()
         var coworkerList = mutableListOf<User>()
+    }
+
+    override fun onCreate(){
+        super.onCreate()
+        instance = this
+        myContext = this
     }
 }

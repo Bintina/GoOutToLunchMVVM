254d579667ca7ceebd54e30f768f2a4e1c28c63f
Bintina <105106238+Bintina@users.noreply.github.com>
2024-04-23 10:49:17 +0300

    Login Injection userDao instantiated correctly.

--- app/src/main/java/com/bintina/goouttolunchmvvm/login/viewmodel/injection/Injection.kt
+++ app/src/main/java/com/bintina/goouttolunchmvvm/login/viewmodel/injection/Injection.kt
@@ -1,7 +1,9 @@
 package com.bintina.goouttolunchmvvm.login.viewmodel.injection
 
 import android.content.Context
+import com.bintina.goouttolunchmvvm.MyApp
 import com.bintina.goouttolunchmvvm.login.model.database.SaveUserDatabase
+import com.bintina.goouttolunchmvvm.login.model.database.UserDao
 import com.bintina.goouttolunchmvvm.login.model.database.repositories.UserDataRepository
 import com.bintina.goouttolunchmvvm.login.viewmodel.ViewModelFactory
 import java.util.concurrent.Executor
@@ -14,13 +16,15 @@ object Injection {
         return UserDataRepository(database!!.userDao())
     }
 
-    private fun provideExecutor(): Executor{
+    fun provideExecutor(): Executor{
         return Executors.newSingleThreadExecutor()
     }
 
     fun provideViewModelFactory(context: Context): ViewModelFactory{
         val dataSourceUser = provideUserDataSource(context)
         val executor = provideExecutor()
-        return ViewModelFactory(dataSourceUser,executor)
+        val userDao = SaveUserDatabase.getInstance(context).userDao()
+        val application = MyApp()
+        return ViewModelFactory(application, dataSourceUser, userDao, executor)
     }
 }

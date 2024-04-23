e69a69c448af232f4f25c68bccc309d3dc033fa5
Bintina <105106238+Bintina@users.noreply.github.com>
2024-04-23 10:51:59 +0300

    ViewModel and ViewModelFactory updated to instantiate ViewModel variables

--- app/src/main/java/com/bintina/goouttolunchmvvm/login/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/bintina/goouttolunchmvvm/login/viewmodel/UserViewModel.kt
@@ -1,20 +1,53 @@
 package com.bintina.goouttolunchmvvm.login.viewmodel
 
+import android.app.Application
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.bintina.goouttolunchmvvm.MyApp
+import com.bintina.goouttolunchmvvm.R
 import com.bintina.goouttolunchmvvm.login.model.User
+import com.bintina.goouttolunchmvvm.login.model.database.dao.UserDao
 import com.bintina.goouttolunchmvvm.login.model.database.repositories.UserDataRepository
+import com.bintina.goouttolunchmvvm.login.view.MyLogInFragment
+import com.bintina.goouttolunchmvvm.login.viewmodel.injection.Injection
+import com.facebook.login.LoginFragment
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import java.util.concurrent.Executor
 
-class UserViewModel (private val userDataSource: UserDataRepository, private val executor: Executor): ViewModel(){
+
+class UserViewModel(
+    application: Application,
+    private val userDataSource: UserDataRepository,
+    private val userDao: UserDao,
+    val executor: Executor
+) : ViewModel() {
+
+    lateinit var vmExecutor: Executor
+    lateinit var vmViewModel: ViewModel
 
     private var currentUser: LiveData<User>? = null
+    private val context = MyApp.myContext
+    private val vmViewModelFactory: ViewModelFactory = Injection.provideViewModelFactory(context)
+    private val viewModelScope: CoroutineScope = CoroutineScope(Dispatchers.IO)
+    val vmLogInFragment = MyLogInFragment()
+    val KEY_LOGIN_FRAGMENT = "KEY_LOGIN_FRAGMENT"
+    val mainContainerInt = R.id.main_fragment_container
+    val vmUserDao = userDao
 
-    fun init(userId: Long){
-        if (currentUser != null){
+
+    init {
+        vmExecutor = Injection.provideExecutor()
+        vmViewModel = UserViewModel(application, UserDataRepository(userDao), userDao, vmExecutor )
+    }
+
+
+    fun init(userId: Long) {
+        if (currentUser != null) {
             return
         }
         currentUser = userDataSource.getUser(userId)
+
     }
 
     //For User
--- app/src/main/java/com/bintina/goouttolunchmvvm/login/viewmodel/ViewModelFactory.kt
+++ app/src/main/java/com/bintina/goouttolunchmvvm/login/viewmodel/ViewModelFactory.kt
@@ -1,19 +1,27 @@
 package com.bintina.goouttolunchmvvm.login.viewmodel
 
+import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.bintina.goouttolunchmvvm.MyApp
 import com.bintina.goouttolunchmvvm.login.model.User
+import com.bintina.goouttolunchmvvm.login.model.database.dao.UserDao
 import com.bintina.goouttolunchmvvm.login.model.database.repositories.UserDataRepository
 import java.util.concurrent.Executor
 import javax.sql.DataSource
 
 
-class ViewModelFactory(private val userDataSource: UserDataRepository, private val executor: Executor):ViewModelProvider.Factory {
+class ViewModelFactory(
+    val application: Application,
+    private val userDataSource: UserDataRepository,
+    private val userDao: UserDao,
+    private val executor: Executor
+) : ViewModelProvider.Factory {
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return if (modelClass.isAssignableFrom(UserViewModel::class.java)){
-            UserViewModel(userDataSource, executor) as T
+        return if (modelClass.isAssignableFrom(UserViewModel::class.java)) {
+            UserViewModel(application, userDataSource, userDao, executor) as T
         } else {
             throw IllegalArgumentException("Unknown ViewModel class")
         }

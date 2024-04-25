758421aac23fcb69aa9ca0af6ac9bad245aa6587
Bintina <105106238+Bintina@users.noreply.github.com>
2024-04-25 19:01:23 +0300

    First ViewModelTests

---
+++ app/src/androidTest/java/com/bintina/goouttolunchmvvm/UserViewModelInstrumentedTest.kt
@@ -0,0 +1,44 @@
+package com.bintina.goouttolunchmvvm
+
+import android.content.Context
+import androidx.fragment.app.testing.FragmentScenario
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.bintina.goouttolunchmvvm.user.login.view.MyLogInFragment
+import com.bintina.goouttolunchmvvm.user.login.viewmodel.UserViewModel
+import com.bintina.goouttolunchmvvm.user.model.database.dao.UserDao
+import com.bintina.goouttolunchmvvm.user.model.database.repositories.UserDataRepository
+import com.bintina.goouttolunchmvvm.utils.MyApp
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito.mock
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
+
+
+@RunWith(AndroidJUnit4::class)
+class UserViewModelInstrumentedTest {
+    private lateinit var scenario: FragmentScenario<MyLogInFragment>
+    private lateinit var context: Context
+    private lateinit var viewModel: UserViewModel
+
+    @Before
+    fun setUp() {
+        scenario = launchFragmentInContainer<MyLogInFragment>()
+        context = ApplicationProvider.getApplicationContext<Context>()
+        val application = MyApp.instance
+        val executors = Executors.newSingleThreadExecutor() // Example executor
+        val userDao: UserDao = mock(UserDao::class.java)
+        val mockUserDataSource = UserDataRepository(userDao)
+        viewModel = UserViewModel(application, mockUserDataSource, userDao, executors)
+    }
+
+
+    @Test
+    fun user_view_model_facebook_button_is_initialized() {
+        viewModel.vmLogInFragment.viewModel.facebookLoginBtn.isInitialized
+
+    }
+}
--- app/src/test/java/com/bintina/goouttolunchmvvm/user/login/viewmodel/UserViewModelTest.kt
+++ app/src/test/java/com/bintina/goouttolunchmvvm/user/login/viewmodel/UserViewModelTest.kt
@@ -1,14 +1,26 @@
 package com.bintina.goouttolunchmvvm.user.login.viewmodel
 
+import android.content.Context
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import androidx.test.core.app.ApplicationProvider
+import com.bintina.goouttolunchmvvm.user.model.User
 import com.bintina.goouttolunchmvvm.user.model.database.dao.UserDao
-import com.bintina.goouttolunchmvvm.user.model.database.repositories.MockUser
 import com.bintina.goouttolunchmvvm.user.model.database.repositories.UserDataRepository
-import com.bintina.goouttolunchmvvm.utils.MyApp
+import com.google.android.gms.common.internal.safeparcel.SafeParcelable
+import org.junit.Test
+import org.mockito.Mockito.mock
+import java.lang.reflect.TypeVariable
 
 
 class UserViewModelTest{
-    //application,
-    private val userDao = UserDao
-    private val userDataSource = UserDataRepository(userDao)
-    private val viewModel = UserViewModel(userDataSource, userDao)
+    val context = ApplicationProvider.getApplicationContext<Context>()
+    private val userDao: UserDao = mock(UserDao::class.java)
+    private val mockUserDataSource = UserDataRepository(userDao)
+    private val viewModel = UserViewModel(context, mockUserDataSource, userDao)
+
+    @Test
+    fun user_view_model_facebook_button_is_initialized(){
+        viewModel.vmLogInFragment.viewModel.facebookLoginBtn.isInitialized
+
+    }
 }

b5217165a344c762e21d38cc8b56f7a917664ed2
Bintina <105106238+Bintina@users.noreply.github.com>
2024-04-29 10:29:49 +0300

    Navigation Dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.annotation:annotation:1.7.1")
+    implementation("androidx.test:core-ktx:1.5.0")
 
     //Testing
     testImplementation("junit:junit:4.13.2")
@@ -65,6 +66,16 @@ dependencies {
     // Navigation Component
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    // Kotlin
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+
+    // Feature module Support
+    implementation("androidx.navigation:navigation-dynamic-features-fragment:2.7.7")
+
+    // Testing Navigation
+    androidTestImplementation("androidx.navigation:navigation-testing:2.7.7")
+
 
     //Room
     implementation("androidx.room:room-runtime:2.6.1")

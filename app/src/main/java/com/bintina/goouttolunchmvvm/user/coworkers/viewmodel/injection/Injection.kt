b06c2c9e9a9386a9630ee2b87e011091e75f3d89
Bintina <105106238+Bintina@users.noreply.github.com>
2024-05-02 10:00:14 +0300

    Coworker ViewModel corrections

--- app/src/main/java/com/bintina/goouttolunchmvvm/user/coworkers/viewmodel/injection/Injection.kt
+++ app/src/main/java/com/bintina/goouttolunchmvvm/user/coworkers/viewmodel/injection/Injection.kt
@@ -33,7 +33,7 @@ object Injection {
         val executor = provideExecutor()
         val userDao = SaveUserDatabase.getInstance(context).userDao()
         val application = MyApp() // Assuming MyApp extends Application
-        val factory = ViewModelFactory(application, dataSourceUser, userDao, executor)
+        val factory = ViewModelFactory(userDao)
         return factory.create(CoworkersViewModel::class.java)
     }
 }

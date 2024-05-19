5189a138fac8fc9defb2dbf1187889276cc9c0e1
Bintina <105106238+Bintina@users.noreply.github.com>
2024-05-19 11:02:53 +0300

    Api data fetching formating applied

--- app/src/main/java/com/bintina/goouttolunchmvvm/restaurants/model/api/ApiClient.kt
+++ app/src/main/java/com/bintina/goouttolunchmvvm/restaurants/model/api/ApiClient.kt
@@ -5,8 +5,13 @@ import com.bintina.goouttolunchmvvm.utils.Constants.GEOLOCATE_END_URL
 import com.bintina.goouttolunchmvvm.utils.Constants.PLACE_END_URL
 import com.bintina.goouttolunchmvvm.utils.MyApp
 import retrofit2.http.GET
+import retrofit2.http.Headers
 
 interface ApiClient {
+    @Headers(
+        "Accept: application/json",
+        "Content-Type: application/json"
+    )
     @GET(PLACE_END_URL)
     suspend fun getRestaurants(): com.bintina.goouttolunchmvvm.restaurants.model.database.responseclasses.RestaurantResult
 }
--- app/src/main/java/com/bintina/goouttolunchmvvm/restaurants/model/database/repository/DataSource.kt
+++ app/src/main/java/com/bintina/goouttolunchmvvm/restaurants/model/database/repository/DataSource.kt
@@ -5,30 +5,31 @@ import androidx.lifecycle.LifecycleCoroutineScope
 import com.bintina.goouttolunchmvvm.restaurants.model.Restaurant
 import com.bintina.goouttolunchmvvm.restaurants.model.api.ApiService
 import com.bintina.goouttolunchmvvm.utils.MyApp
-import com.bintina.goouttolunchmvvm.utils.MyApp.Companion.restaurantList
+
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 
 object DataSource {
 
-    suspend fun loadRestaurantList(lifecycleScope: LifecycleCoroutineScope): List<com.bintina.goouttolunchmvvm.restaurants.model.database.responseclasses.Restaurant?>{
+    suspend fun loadRestaurantList(lifecycleScope: LifecycleCoroutineScope): List<com.bintina.goouttolunchmvvm.restaurants.model.database.responseclasses.Restaurant?> {
         val restaurantsDeferred = loadRestaurants(lifecycleScope)
 
         val restaurantResult = restaurantsDeferred.await()
 
-        return restaurantResult.toList()
+        return restaurantResult.toMutableList()
     }
 
-    suspend fun loadRestaurants(lifecycleScope: LifecycleCoroutineScope): Deferred<List<com.bintina.goouttolunchmvvm.restaurants.model.database.responseclasses.Restaurant?>> = lifecycleScope.async(Dispatchers.IO) {
-        val apiCall = ApiService.create()
+    suspend fun loadRestaurants(lifecycleScope: LifecycleCoroutineScope): Deferred<List<com.bintina.goouttolunchmvvm.restaurants.model.database.responseclasses.Restaurant?>> =
+        lifecycleScope.async(Dispatchers.IO) {
+            val apiCall = ApiService.create()
 
-        val response = apiCall.getRestaurants()
+            val response = apiCall.getRestaurants()
 
-        val result = response.results
+            val result = response.results
 
-        MyApp.restaurantList = result
-        Log.d("DataSourceRestaurantsLog", "loaded ${restaurantList.size}")
-        return@async restaurantList
-    }
+            MyApp.restaurantList = result
+            Log.d("DataSourceRestaurantsLog", "loaded ${result.size}")
+            return@async result
+        }
 }

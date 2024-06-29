3ebe5c028f2a976ea7268839009eebb5b3a6fdbb
Bintina <105106238+Bintina@users.noreply.github.com>
2024-06-29 18:24:23 +0300

    UI data handling

--- app/src/main/java/com/bintina/goouttolunchmvvm/user/model/LocalUser.kt
+++ app/src/main/java/com/bintina/goouttolunchmvvm/user/model/LocalUser.kt
@@ -4,6 +4,7 @@ package com.bintina.goouttolunchmvvm.user.model
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.bintina.goouttolunchmvvm.restaurants.model.LocalRestaurant
 
 @Entity()
 data class LocalUser(
@@ -15,6 +16,7 @@ data class LocalUser(
     val email: String,
     @ColumnInfo(name = "profile_picture_url")
     val profilePictureUrl: String? = null,
+    var attending: LocalRestaurant?,
     val createdAt: Long,
     val updatedAt: Long
     //var restaurant: Restaurant? = null

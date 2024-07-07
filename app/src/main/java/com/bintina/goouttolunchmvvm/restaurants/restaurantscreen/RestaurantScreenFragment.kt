8a6cbaf7b4044217f59255992e9a40b2ff574b69
Bintina <105106238+Bintina@users.noreply.github.com>
2024-07-07 12:33:30 +0300

    RestaurantScreen onClick set outside of recyclerView. Adapter configurations

--- app/src/main/java/com/bintina/goouttolunchmvvm/restaurants/restaurantscreen/RestaurantScreenFragment.kt
+++ app/src/main/java/com/bintina/goouttolunchmvvm/restaurants/restaurantscreen/RestaurantScreenFragment.kt
@@ -1,26 +1,35 @@
 package com.bintina.goouttolunchmvvm.restaurants.restaurantscreen
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bintina.goouttolunchmvvm.databinding.FragmentRestaurantScreenBinding
 import com.bintina.goouttolunchmvvm.restaurants.model.LocalRestaurant
 import com.bintina.goouttolunchmvvm.restaurants.restaurantscreen.adapter.Adapter
 import com.bintina.goouttolunchmvvm.restaurants.viewmodel.RestaurantViewModel
+import com.bintina.goouttolunchmvvm.restaurants.viewmodel.confirmAttending
 import com.bintina.goouttolunchmvvm.user.model.LocalUser
 import com.bintina.goouttolunchmvvm.user.viewmodel.injection.Injection
+import com.bintina.goouttolunchmvvm.utils.CurrentUserRestaurant
+import com.bintina.goouttolunchmvvm.utils.MyApp
+import com.bintina.goouttolunchmvvm.utils.MyApp.Companion.currentUserRestaurant
 import com.bintina.goouttolunchmvvm.utils.loadImage
 
-class RestaurantScreenFragment: Fragment(), OnAttendingClickedListener {
 
+class RestaurantScreenFragment : Fragment() {
+
+    private val TAG = "RestaurantScreenFragLog"
     private var _binding: FragmentRestaurantScreenBinding? = null
     private val binding get() = _binding!!
     lateinit var adapter: Adapter
     private lateinit var viewModel: RestaurantViewModel
+    lateinit var currentRestaurant: CurrentUserRestaurant
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -34,16 +43,38 @@ class RestaurantScreenFragment: Fragment(), OnAttendingClickedListener {
                 RestaurantViewModel::class.java
             )
 
+        binding.attendingButton.setOnClickListener {
+
+            Log.d(TAG, " currentUserRestaurant is ${MyApp.currentClickedRestaurant}")
+            confirmAttending(MyApp.currentClickedRestaurant!!)
+            Log.d(TAG, " currentUserRestaurant is ${MyApp.currentClickedRestaurant} after click")
+        }
+
         initializeViews()
         return binding.root
     }
 
     private fun initializeViews() {
+        Log.d(
+            TAG,
+            "viewModel.currenClickedRestaurant is ${MyApp.currentClickedRestaurant}"
+        )
+        currentRestaurant = MyApp.currentClickedRestaurant!!
+        Log.d(TAG, "currentRestaurant is $currentRestaurant")
+        binding.attendanceRecycler.layoutManager = LinearLayoutManager(requireContext())
+
+        binding.restaurantName.text = currentRestaurant.restaurantName
+        binding.restaurantStyleAndAddress.text = "Some address"
+
         adapter = Adapter()
-        loadImage("${viewModel.currentRestaurant?.photoUrl}", binding.restaurantImage)
-        binding.restaurantName.text = viewModel.currentRestaurant?.name
-        binding.restaurantStyleAndAddress.text = /*${viewModel.currentRestaurant?.style} -*/" ${viewModel.currentRestaurant?.address}"
-        adapter.attendingList = viewModel.currentRestaurantAttendingList
+        Log.d(TAG, "profile photo url is ${currentRestaurant.restaurantPictureUrl}")
+        loadImage("${currentRestaurant.restaurantPictureUrl}", binding.restaurantImage)
+        binding.attendanceRecycler.adapter = adapter
+        viewModel.restaurantList.observe(viewLifecycleOwner, { restaurantList ->
+            adapter.attendingList = currentRestaurant.attending
+            Log.d(TAG, "attendingList is ${adapter.attendingList}")
+            adapter.notifyDataSetChanged()
+        })
     }
 
     override fun onDestroyView() {
@@ -51,7 +82,4 @@ class RestaurantScreenFragment: Fragment(), OnAttendingClickedListener {
         _binding = null
     }
 
-    override fun onAttendingClick(currentUser: LocalUser, restaurant: LocalRestaurant) {
-        TODO("Not yet implemented")
-    }
 }

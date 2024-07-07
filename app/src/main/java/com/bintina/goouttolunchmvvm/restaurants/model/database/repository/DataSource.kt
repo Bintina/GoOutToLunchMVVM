eef67fb8b90158f5dcea606aab1ed5b6e44d9d5e
Bintina <105106238+Bintina@users.noreply.github.com>
2024-07-07 12:38:03 +0300

    Adjusted database repo loading method

--- app/src/main/java/com/bintina/goouttolunchmvvm/restaurants/model/database/repository/DataSource.kt
+++ app/src/main/java/com/bintina/goouttolunchmvvm/restaurants/model/database/repository/DataSource.kt
@@ -3,6 +3,7 @@ package com.bintina.goouttolunchmvvm.restaurants.model.database.repository
 import android.util.Log
 import androidx.lifecycle.LifecycleCoroutineScope
 import com.bintina.goouttolunchmvvm.restaurants.model.api.ApiService
+import com.bintina.goouttolunchmvvm.restaurants.model.database.responseclasses.Restaurant
 import com.bintina.goouttolunchmvvm.utils.MyApp
 import kotlinx.coroutines.CoroutineScope
 
@@ -12,12 +13,18 @@ import kotlinx.coroutines.async
 
 object DataSource {
 
-    suspend fun loadRestaurantList(lifecycleScope: CoroutineScope): List<com.bintina.goouttolunchmvvm.restaurants.model.database.responseclasses.Restaurant?> {
+    suspend fun loadRestaurantList(lifecycleScope: CoroutineScope): List<com.bintina.goouttolunchmvvm.restaurants.model.database.responseclasses.Restaurant> {
+        var populatedRestaurantResult = emptyList<Restaurant>()
+        try{
         val restaurantsDeferred = loadRestaurants(lifecycleScope)
 
         val restaurantResult = restaurantsDeferred.await()
+            populatedRestaurantResult = restaurantResult.filterNotNull()
+        } catch (e: Exception) {
+            Log.d("DataSourceRestaurantsLog", "loadRestaurantList failed: ${e.message}")
+        }
 
-        return restaurantResult.toMutableList()
+        return populatedRestaurantResult
     }
 
     suspend fun loadRestaurants(lifecycleScope: CoroutineScope): Deferred<List<com.bintina.goouttolunchmvvm.restaurants.model.database.responseclasses.Restaurant?>> =

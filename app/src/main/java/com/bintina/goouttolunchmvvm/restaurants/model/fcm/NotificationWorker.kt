72e3d345706ecc95e0246f99b75649235bbefb07
Bintina <105106238+Bintina@users.noreply.github.com>
2024-07-25 02:19:10 +0300

    Place holder worker class

---
+++ app/src/main/java/com/bintina/goouttolunchmvvm/restaurants/model/fcm/NotificationWorker.kt
@@ -0,0 +1,16 @@
+package com.bintina.goouttolunchmvvm.restaurants.model.fcm
+
+import android.content.Context
+import android.util.Log
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+class NotificationWorker(appContext: Context, workerParams: WorkerParameters): Worker(appContext, workerParams) {
+
+    val TAG = "NotificationWorkerLog"
+    override fun doWork(): Result {
+        Log.d(TAG, "Performing notification work doWork()")
+        //TODO("Work")
+        return Result.success()
+    }
+}

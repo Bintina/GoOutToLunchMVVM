ee439c5a785ecd1f429a7d3e7d31d4316a8f0298
Bintina <105106238+Bintina@users.noreply.github.com>
2024-08-16 16:13:42 +0300

    Drapht work and notificaiton testing.

---
+++ app/src/test/java/com/bintina/goouttolunchmvvm/userwithrestaurant/FCMTest.kt
@@ -0,0 +1,13 @@
+package com.bintina.goouttolunchmvvm.userwithrestaurant
+
+/*
+class FCMTest {
+
+    // Simulate FCM message received in your app
+    val mockData = mapOf(
+        "title" to "Test Title",
+        "body" to "Test Body"
+    )
+// Call your FCM service handling method
+    NotificationFirebaseMessagingService.handleIncomingData(mockData)
+}*/
---
+++ app/src/test/java/com/bintina/goouttolunchmvvm/userwithrestaurant/ResetWorkTest.kt
@@ -0,0 +1,23 @@
+package com.bintina.goouttolunchmvvm.userwithrestaurant
+
+import android.app.Application
+import com.bintina.goouttolunchmvvm.restaurants.work.ResetWork
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ResetWorkTest {
+  /*  @Test
+    fun testResetWork() = runBlocking {
+        // Mock or spy methods
+        val appContext = Application.CONTEXT_INCLUDE_CODE
+        val worker = ResetWork(appContext, workerParams)
+
+        // Simulate execution
+        val result = worker.doWork()
+
+        // Validate the result
+        assertEquals(Result.success(), result)
+        // Ensure mock methods were called
+    }*/
+}
